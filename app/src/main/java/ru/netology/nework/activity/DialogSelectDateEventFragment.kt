3b8728066c47496cea89c20db24d5e9d82eb7fbe
Igor Repin <repin_iv@vk.com>
2024-03-23 13:07:45 +0500

    add fragments dialog

---
+++ app/src/main/java/ru/netology/nework/activity/DialogSelectDateEventFragment.kt
@@ -0,0 +1,7 @@
+package ru.netology.nework.activity
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class DialogSelectDateEventFragment : BottomSheetDialogFragment() {
+
+}
